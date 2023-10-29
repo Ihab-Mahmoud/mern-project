22a6b2323184066765bf60758d3619ab0117d7f3
Ihab Mahmoud <hakemehab22@gmail.com>
2023-10-29 21:22:12 +0300

    TEST

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: [".js", ".ts", ".vue", ".json"],
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5010/api',
+      "/api": {
+        target: "http://localhost:5010/api",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
